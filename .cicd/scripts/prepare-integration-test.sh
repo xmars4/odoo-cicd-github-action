0f6f5d575fcc0731efa68e4f50d7a5750d4c43ed
XMars <truong.pham2@sotatek.com>
2023-11-27 15:54:37 +0700

    update

---
+++ .cicd/scripts/prepare-integration-test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+source "${PIPELINE_UTILS_SCRIPT_PATH}"
+
+function update_config_file {
+    sed -i "s/^\s*command\s*.*//g" $CONFIG_FILE
+    sed -i "s/^\s*db_name\s*.*//g" $CONFIG_FILE
+}
+
+function main() {
+    update_config_file
+    copy_requirements_txt_file
+}
+
+main "$@"
