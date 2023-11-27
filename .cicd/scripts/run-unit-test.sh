dac9fa0f89e1be6efdbb86c190de4904239026a1
XMars <truong.pham2@sotatek.com>
2023-11-27 13:44:09 +0700

    update

--- .cicd/scripts/run-unit-test.sh
+++ .cicd/scripts/run-unit-test.sh
@@ -6,6 +6,7 @@ show_separator "Start analyzing log file"
 function main() {
     type=$1
     wait_until_odoo_shutdown
+    cat $CONFIG_FILE
     failed_message=$(
         cat <<EOF
 🐞 $type: A few unit test cases for the [PR \\#$PR_NUMBER]($PR_URL) did not pass\\! 🐞
