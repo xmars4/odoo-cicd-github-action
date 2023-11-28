f22bfa686e1f59ba96081cb301fa2c7dd567026b
XMars <truong.pham2@sotatek.com>
2023-11-28 17:35:33 +0700

    update

--- pos_discount_all/models/pos_session.py
+++ pos_discount_all/models/pos_session.py
@@ -6,7 +6,7 @@ from odoo import models
 
 
 class PosSession(models.Model):
-    _inherit = "pos.session1"
+    _inherit = "pos.session"
 
     def _loader_params_product_product(self):
         res = super()._loader_params_product_product()
