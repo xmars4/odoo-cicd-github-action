b3dc6889b9586448ed942cbd1869d2b2ad34526c
XMars <truong.pham2@sotatek.com>
2023-11-29 14:41:07 +0700

    [FIX] anonymous: update CICD

--- pos_discount_all/models/pos_session.py
+++ pos_discount_all/models/pos_session.py
@@ -1,6 +1,6 @@
 # Copyright (C) 2022-Today GRAP (http://www.grap.coop)
 # @author Sylvain LE GAL (https://twitter.com/legalsylvain)
-# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html.
 
 from odoo import models
 
