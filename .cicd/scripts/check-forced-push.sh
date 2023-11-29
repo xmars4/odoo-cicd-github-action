ffe39d3573e53aa8281cc1dcf6a93c7f91317004
XMars <truong.pham2@sotatek.com>
2023-11-29 11:16:36 +0700

    update

--- .cicd/scripts/check-forced-push.sh
+++ .cicd/scripts/check-forced-push.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 source "${CICD_UTILS_SCRIPTS_PATH}"
 
-message=$(
-    cat <<EOF
+function main {
+    if [[ $EVENT_NAME == 'push' ]]; then
+        message=$(
+            cat <<EOF
 \\@${PUSH_USERNAME}
 ⛔⛔ You are not allowed to forced push to branch ${PUSH_BRANCH_NAME}\\! ⛔⛔
 Please make a pull request to branch ${PUSH_BRANCH_NAME} instead
 EOF
-)
-send_message_telegram_default "$message"
-exit 1
+        )
+        send_message_telegram_default "$message"
+        exit 1
+    fi
+}
