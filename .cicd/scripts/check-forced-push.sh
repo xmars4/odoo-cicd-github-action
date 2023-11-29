ea6894c6a7d4350164b00098847515c27aaa2e9e
XMars <truong.pham2@sotatek.com>
2023-11-29 11:12:41 +0700

    update

--- .cicd/scripts/check-forced-push.sh
+++ .cicd/scripts/check-forced-push.sh
@@ -3,8 +3,8 @@ source "${CICD_UTILS_SCRIPTS_PATH}"
 
 message=$(
     cat <<EOF
-${PUSH_USERNAME}
-⛔⛔⛔ You are not allowed to forced push to branch ${PUSH_BRANCH_NAME}\\! ⛔⛔⛔
+\\@${PUSH_USERNAME}
+⛔⛔ You are not allowed to forced push to branch ${PUSH_BRANCH_NAME}\\! ⛔⛔
 Please make a pull request to branch ${PUSH_BRANCH_NAME} instead
 EOF
 )
