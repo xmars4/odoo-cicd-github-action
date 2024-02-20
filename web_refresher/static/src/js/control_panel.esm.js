edb2af121edcbef4b1b5c5e02db3469b7b57dd15
XMars <truong@vdx.vn>
2024-02-20 14:29:55 +0700

    [IMP] anonymous: add module

--- web_refresher/static/src/js/control_panel.esm.js
+++
@@ -1,9 +0,0 @@
-/** @odoo-module **/
-/* Copyright 2024 Tecnativa - Carlos Roca
- * License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl). */
-import {ControlPanel} from "@web/search/control_panel/control_panel";
-import {Refresher} from "./refresher.esm";
-
-ControlPanel.components = Object.assign({}, ControlPanel.components, {
-    Refresher,
-});
