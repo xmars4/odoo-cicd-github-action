f7c15d978c7730ad263609ad5f3330dab720a354
XMars <truong@vdx.vn>
2024-02-22 10:25:19 +0700

    up

--- stock_no_negative/__manifest__.py
+++ stock_no_negative/__manifest__.py
@@ -10,6 +10,7 @@
     "license": "AGPL-3",
     "summary": "Disallow negative stock levels by default",
     "author": "Akretion,Odoo Community Association (OCA)",
+    "what": "heaven",
     "website": "https://github.com/OCA/stock-logistics-workflow",
     "depends": ["stock"],
     "data": ["views/product_product_views.xml", "views/stock_location_views.xml"],
